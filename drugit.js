1a80e05e2db865a943658ddfeb0737be36ebf9d9
Akira <arsen1491@gmail.com>
2014-10-05 20:57:56 +0600

    0.1

--- drugit.js
+++ drugit.js
@@ -1,28 +1,33 @@
 ﻿$(document).ready(function(){
 	var d = $('.drugit');
+	var flag;
 	d.mousedown(function(){
-		var IE = document.all?true:false
-		if (!IE) document.captureEvents(Event.MOUSEMOVE)
-		document.onmousemove = getMouseXY;
-		var tempX = 0;
-		var tempY = 0;
-		function getMouseXY(e) {
-			if (IE) {
-				tempX = event.clientX + document.body.scrollLeft
-				tempY = event.clientY + document.body.scrollTop
-			}else{
-				tempX = e.pageX
-				tempY = e.pageY
+		flag = true;
+		if(flag == true){
+			var IE = document.all?true:false
+			if (!IE) document.captureEvents(Event.MOUSEMOVE)
+			document.onmousemove = getMouseXY;
+			var tempX = 0;
+			var tempY = 0;
+			function getMouseXY(e) {
+				if (IE) {
+					tempX = event.clientX + document.body.scrollLeft
+					tempY = event.clientY + document.body.scrollTop
+				}else{
+					tempX = e.pageX
+					tempY = e.pageY
+				}
+				if (tempX < 0){tempX = 0}
+				if (tempY < 0){tempY = 0}
+				d.css('position', 'absolute');
+				d.css('top', tempY);
+				d.css('left', tempX);
 			}
-			if (tempX < 0){tempX = 0}
-			if (tempY < 0){tempY = 0}
-			d.css('position', 'absolute');
-			d.css('top', tempY);
-			d.css('left', tempX);
+			console.log('down');
 		}
-		console.log('down');
 	});
 	$(document).mouseup(function(){
+		flag = false;
 		console.log('up');
 		//	
 	});
